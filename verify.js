1c3d080510ffee38e7bcf7ec43bdededc7c4fd9d
stillbad <59036328+stillbad@users.noreply.github.com>
2020-02-04 13:58:26 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,2 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var video = document.getElementById('video'), 
+captureCanvas = document.getElementById('captureCanvas'), 
+uploadCanvas = document.getElementById('uploadCanvas'), 
+captureContext = captureCanvas.getContext('2d'),
+uploadContext = uploadCanvas.getContext('2d'),
+uploadedPhoto = document.getElementById('uploadedPhoto'),
+capturedPhoto = document.getElementById('capturedPhoto'),
+imageUploadInput = document.querySelector('[name="image-upload"]'),
+apiKey = 'INSERT_YOUR_FACESOFT_API_KEY',
+errorAlert = document.getElementById('errorAlert'), AlertwarningAlert = document.getElementById('warningAlert'),
+matchText = document.getElementById('match'),
+scoreText = document.getElementById('score');
+  // Stream Camera To Video Element
+if(navigator.mediaDevices.getUserMedia){
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(function(stream) {
+    video.srcObject = stream;
+  }).catch(function(error) {
+    console.log(error)
+  })
+}
 });
